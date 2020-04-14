b391ce92c1dca023b7501c443c8c7331fff139e9
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-14 19:34:44 +0200

    Initial GUI implemented

--- src/com/michalgarnczarski/Controller.java
+++ src/com/michalgarnczarski/Controller.java
@@ -3,12 +3,18 @@ package com.michalgarnczarski;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
 
 public class Controller {
     @FXML
     private GridPane gridPane;
+    @FXML
+    private Label outputLabel;
 
     public void initialize() {
         gridPane.setAlignment(Pos.TOP_LEFT);
+
+        // wywalić
+        outputLabel.setText("Przykładowy wynik");
     }
 }
