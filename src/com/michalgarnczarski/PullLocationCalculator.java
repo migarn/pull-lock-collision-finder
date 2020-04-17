0578519bdf025d0670bbc76cad8d9f2977b672f5
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-17 18:47:52 +0200

    defineLocationMode method added

--- src/com/michalgarnczarski/PullLocationCalculator.java
+++ src/com/michalgarnczarski/PullLocationCalculator.java
@@ -1,4 +1,20 @@
 package com.michalgarnczarski;
 
 public class PullLocationCalculator {
+    private int sashHeight;
+    private Pull pull;
+
+    public PullLocationCalculator(int sashHeight, Pull pull) {
+        this.sashHeight = sashHeight;
+        this.pull = pull;
+    }
+
+    public int defineLocationMode() {
+        if ((this.sashHeight - pull.getFixingsSpacing()) / 2.0 > 860.0) {
+            // 1 means 'standard' mode
+            return 1;
+        }
+        // 0 means 'symmetrical' mode
+        return 0;
+    }
 }
