5c6c2bda15509ff9dea5fbfc52ea135c8be2d829
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-01 18:50:37 +0200

    readLines method added

--- src/com/michalgarnczarski/LocksParser.java
+++ src/com/michalgarnczarski/LocksParser.java
@@ -1,12 +1,37 @@
 package com.michalgarnczarski;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocksParser {
 
-    public LocksList parseLocksFromFile(String path) {
+    public LocksList parseLocksFromFile(String filename) {
+
+        // Description
+
         LocksList locksList = new LocksList();
 
 
 
+
         return locksList;
+    }
+
+    private List<String> readLines(String filename) throws IOException {
+
+        // Method reads file and returns it as list
+
+        FileReader fileReader = new FileReader(filename);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> lines = new ArrayList<>();
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            lines.add(line);
+        }
+        bufferedReader.close();
+        return lines;
     }
 }
