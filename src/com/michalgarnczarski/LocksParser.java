a13f9f217f2c6865c687a81820bda582d5b85453
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-02 19:06:04 +0200

    LockParser commented and incorrect data handling implemented

--- src/com/michalgarnczarski/LocksParser.java
+++ src/com/michalgarnczarski/LocksParser.java
@@ -1,5 +1,7 @@
 package com.michalgarnczarski;
 
+import com.sun.media.sound.InvalidDataException;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,57 +12,72 @@ public class LocksParser {
 
     public LocksList parseLocksFromFile(String filename) throws IOException {
 
-        // Description
+        // Method parses locks list (LocksList object) from file. File should look like:
+        //
+        //      lock name;cassette width,cassette offset;cassette width,cassette offset
+        //      lock name;cassette width,cassette offset
+        //      lock name;cassette width,cassette offset;cassette width,cassette offset;cassette width,cassette offset
+        //
+        // Each cassette should be separated by ";" symbol.
+        // Method handles basic problems with file.
 
         LocksList locksList = new LocksList();
         List<String> fileAsStringsList = readLines(filename);
 
         for (String line : fileAsStringsList) {
-
-            // Tu docelowo obsłużyć błędną linię. Try parseLock?
-
             Lock lock = parseLock(line);
+
+            if (lock != null )
+
             locksList.addLock(lock);
         }
-
-
-
 
         return locksList;
     }
 
     private List<String> readLines(String filename) throws IOException {
 
-        // Method reads file and returns it as list
+        // Method reads file and returns it as list.
 
         FileReader fileReader = new FileReader(filename);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         List<String> lines = new ArrayList<>();
         String line = null;
+
         while ((line = bufferedReader.readLine()) != null) {
             lines.add(line);
         }
+
         bufferedReader.close();
         return lines;
     }
 
     private Lock parseLock(String line) {
 
-        // Description
+        // Method parses locks from file lines. If line is empty or set incorrectly, method returns null.
 
-        String[] lockParameters = line.split(";");
+        try {
+            String[] lockParameters = line.split(";");
 
-        String lockName = lockParameters[0];
-        LockCassette[] cassettes = new LockCassette[lockParameters.length - 1];
+            if (lockParameters.length == 1) {
+                throw new InvalidDataException();
+            }
 
-        for (int i = 1; i < lockParameters.length; i++) {
-            String[] cassetteParameters = lockParameters[i].split(",");
-            int width = Integer.parseInt(cassetteParameters[0]);
-            int offset = Integer.parseInt(cassetteParameters[1]);
+            String lockName = lockParameters[0];
+            LockCassette[] cassettes = new LockCassette[lockParameters.length - 1];
 
-            cassettes[i - 1] = new LockCassette(width, offset);
+            for (int i = 1; i < lockParameters.length; i++) {
+                String[] cassetteParameters = lockParameters[i].split(",");
+                int width = Integer.parseInt(cassetteParameters[0]);
+                int offset = Integer.parseInt(cassetteParameters[1]);
+
+                cassettes[i - 1] = new LockCassette(width, offset);
+            }
+
+            return new Lock(lockName, cassettes);
+        } catch (Exception e) {
+            System.out.println("Wrong line found.");
+            return null;
         }
-
-        return new Lock(lockName, cassettes);
     }
 }
